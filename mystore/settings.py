d6c190b44d5a0febfe0cd049368ef1fb71fe0751
Amir Tadrisi <amirtds@gmail.com>
2024-04-03 13:31:49 -0500

    chore: adopt the project to run on render

--- mystore/settings.py
+++ mystore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-u8v-!g$^=6%g2g9+^acj6k$h05hdz=72#day&n%lc@1_s)lecj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
